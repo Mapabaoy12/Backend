1bc53ceb9fb8917c53c578ead22e6ffd193255c7
Mapabaoy12 <mat.turra@duocuc.cl>
2025-11-28 01:43:39 -0300

    microgateway

--- Equipo10_Caceres-Turra-main/micro-gateway/src/main/java/com/pasteleria/micro_gateway/MicroGatewayApplication.java
+++ Equipo10_Caceres-Turra-main/micro-gateway/src/main/java/com/pasteleria/micro_gateway/MicroGatewayApplication.java
@@ -1,11 +1,13 @@
-package com.pasteleria.;
+package com.pasteleria.micro_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MicroGatewayApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(MicroGatewayApplication.class, args);
 	}
+
 }
